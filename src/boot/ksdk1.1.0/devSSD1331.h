69d17285f41b1f9d42b1e11225309af01772f433
nikhilantony28 <nikhilantonya@gmail.com>
2021-12-30 22:38:46 +0000

    n

--- src/boot/ksdk1.1.0/devSSD1331.h
+++ src/boot/ksdk1.1.0/devSSD1331.h
@@ -2,6 +2,20 @@
  *	See https://github.com/adafruit/Adafruit-SSD1331-OLED-Driver-Library-for-Arduino for the Arduino driver.
  */
 
+#define NORMAL  0
+#define WIDE    1
+#define HIGH    2
+#define WH      3
+#define WHx36   4
+#define X_width 6 
+#define Y_height 8 
+
+// Screen Settings
+#define WIDTH   96       // Max X axial direction in screen
+#define HEIGHT  64      // Max Y axial direction in screen
+#define width   96-1       // Max X axial direction in screen
+#define height  64-1       // Max Y axial direction in screen
+
 typedef enum
 {
 	kSSD1331ColororderRGB		= 1,
@@ -42,4 +56,24 @@ typedef enum
 	kSSD1331CommandVCOMH		= 0xBE,
 } SSD1331Commands;
 
+static uint8_t lpx;
+static uint8_t lpy;
+// static void FontSizeConvert();
+
+uint8_t chr_size;
+uint8_t char_x;
+uint8_t char_y;
+uint16_t displayedNumber = 0;
+
 int	devSSD1331init(void);
+void writeChar(int value);
+void pixel(uint8_t x,uint8_t y,char colour);
+void locate(uint8_t column, uint8_t row);
+void writeString(const char *pString);
+void writeInt(int* pString, int size);
+void clearScreen(uint8_t x_start, uint8_t y_start,uint8_t x_end,uint8_t y_end);
+
+void display(uint16_t val, uint16_t prevVal);
+int16_t getCurrentDisplay();
+uint16_t countDigits(uint16_t i);
+void splitInt(int *arr, int num);
