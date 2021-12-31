62ce95fcc1777e310a77021510b67781fade47ed
nikhilantony28 <nikhilantonya@gmail.com>
2021-12-31 17:34:42 +0000

    Update devMFRC522.c

--- src/boot/ksdk1.1.0/devMFRC522.c
+++ src/boot/ksdk1.1.0/devMFRC522.c
@@ -8,18 +8,21 @@ https://github.com/asif-mahmud/MIFARE-RFID-with-AVR/tree/master/lib/avr-rfid-lib
 
 
 #include <stdint.h>
+#include "config.h"
 
-#include "fsl_spi_master_driver.h"
-#include "fsl_port_hal.h"
 #include "fsl_misc_utilities.h"
 #include "fsl_device_registers.h"
+#include "fsl_i2c_master_driver.h"
+#include "fsl_spi_master_driver.h"
 #include "fsl_rtc_driver.h"
 #include "fsl_clock_manager.h"
 #include "fsl_power_manager.h"
 #include "fsl_mcglite_hal.h"
+#include "fsl_port_hal.h"
 
-#include "SEGGER_RTT.h"
+#include "errstrs.h"
 #include "gpio_pins.h"
+#include "SEGGER_RTT.h"
 #include "warp.h"
 #include "devMFRC522.h"
 
