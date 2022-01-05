3a7747fc4d0b243485ad8a32118712e7b6edd0d7
nikhilantony28 <nikhilantonya@gmail.com>
2022-01-05 17:14:04 +0000

    Update devDS1307.h

--- src/boot/ksdk1.1.0/devDS1307.h
+++ src/boot/ksdk1.1.0/devDS1307.h
@@ -2,4 +2,4 @@ void		initDS1307(const uint8_t i2cAddress, uint16_t operatingVoltageMillivolts);
 WarpStatus	readSensorRegisterDS1307(uint8_t deviceRegister, int numberOfBytes);
 WarpStatus	writeSensorRegisterDS1307(uint8_t deviceRegister, uint8_t payload); // check
 WarpStatus	configureSensorDS1307(); // edit
-void		printSensorDataDS1307(bool hexModeFlag);
+void		printSensorDataDS1307();
