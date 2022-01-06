86ed985235d8254fcba53cefe9cb499bc194e4ce
nikhilantony28 <nikhilantonya@gmail.com>
2022-01-06 19:04:18 +0000

    Update main_prog.h

--- src/boot/ksdk1.1.0/main_prog.h
+++ src/boot/ksdk1.1.0/main_prog.h
@@ -8,6 +8,6 @@ void		main_printTime();
 void		updateTime();
 bool        timeChange();
 void        showTime();
-uint8_t     checkAlarm(uint8_t *hours, uint8_t *mins);
+uint8_t     checkAlarm(uint8_t *hours, uint8_t *mins, bool alarmOn);
 void readTag();
 bool checkTag(uint64_t savedData);
