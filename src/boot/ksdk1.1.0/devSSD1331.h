445d62e7f75aa4197b9ec161de35c2bbe52c1198
nikhilantony28 <nikhilantonya@gmail.com>
2022-01-06 14:54:46 +0000

    Update devSSD1331.h

--- src/boot/ksdk1.1.0/devSSD1331.h
+++ src/boot/ksdk1.1.0/devSSD1331.h
@@ -82,4 +82,4 @@ void display(uint16_t val, uint16_t prevVal);
 int16_t getCurrentDisplay();
 uint16_t countDigits(uint16_t i);
 void splitInt(int *arr, int num);
-void bottomRECT();
+void bottomRECT(uint8_t red, uint8_t green, uint8_t blue);
