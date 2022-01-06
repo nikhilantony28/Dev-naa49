a9dc76bbe5aa144610e1b864bca67510d78c2678
nikhilantony28 <nikhilantonya@gmail.com>
2022-01-06 14:09:47 +0000

    Update devSSD1331.c

--- src/boot/ksdk1.1.0/devSSD1331.c
+++ src/boot/ksdk1.1.0/devSSD1331.c
@@ -135,7 +135,7 @@ writeCommand(uint8_t commandByte)
 	 *	Make sure there is a high-to-low transition by first driving high, delay, then drive low.
 	 */
 	GPIO_DRV_SetPinOutput(kSSD1331PinCSn);
-	OSA_TimeDelay(10);
+	OSA_TimeDelay(1);
 	GPIO_DRV_ClearPinOutput(kSSD1331PinCSn);
 
 	/*
@@ -169,7 +169,7 @@ writeCommandMulti(uint8_t *commandByte, uint8_t count)
 	 *	Make sure there is a high-to-low transition by first driving high, delay, then drive low.
 	 */
 	GPIO_DRV_SetPinOutput(kSSD1331PinCSn);
-	OSA_TimeDelay(1);
+	OSA_TimeDelay(10);
 	GPIO_DRV_ClearPinOutput(kSSD1331PinCSn);
 
 	/*
