f9446df0cfe502c5ada0e3d65606f67ec834453e
nikhilantony28 <nikhilantonya@gmail.com>
2022-01-07 12:29:34 +0000

    Update main_prog.c

--- src/boot/ksdk1.1.0/main_prog.c
+++ src/boot/ksdk1.1.0/main_prog.c
@@ -34,12 +34,12 @@ uint8_t alarmH[10] = {9,10,10,11,12,13,14,15,16,10};
 uint8_t alarmM[10] = {55,0,2,3,4,5,6,7,8,9};
 char*  pillNames[10] = {"pill1","pill2","pill3","pill4","pill5","pill6","pill7","pill8","pill9","pill10"};
 uint64_t pillCodes[10] = {
+    /*
     0x880404D850,
     0x880495829b,
     0x8804aab395,
     0x8804d5bee7,
     0x880422ba14,
-    /*
     0x8804408b47,
     0x8804938a95,
     0x880454b66e,
