d3511f2276a301462db6e4f5e9953a9798c2d67a
nikhilantony28 <nikhilantonya@gmail.com>
2022-01-09 13:14:59 +0000

    hmm

--- src/boot/ksdk1.1.0/devDS1307.c
+++ src/boot/ksdk1.1.0/devDS1307.c
@@ -89,9 +89,9 @@ WarpStatus
 setTimeDS1307(uint8_t seconds, uint8_t minutes, uint8_t hours)
 {
 	WarpStatus	i2cWriteStatus1, i2cWriteStatus2, i2cWriteStatus3;
-	uint8_t newSeconds = convertHexa2Sexa(seconds);
-	uint8_t newMinutes = convertHexa2Sexa(minutes);
-	uint8_t newHours = convertHexa2Sexa(hours);
+	uint8_t newSeconds = convertHexaSexa(seconds);
+	uint8_t newMinutes = convertHexaSexa(minutes);
+	uint8_t newHours = convertHexaSexa(hours);
 
 	warpScaleSupplyVoltage(deviceDS1307State.operatingVoltageMillivolts);
 	i2cWriteStatus1 = writeSensorRegisterDS1307(0x00 /*set seconds value  */,		newSeconds);
